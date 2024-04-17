78e845032ece88471ea1b50469438fbc147f54d4
Satya Naraharisetty <venkatnaraharisetty1234@gmail.com>
2024-04-17 12:41:32 +0530

    prototype

---
+++ index.js
@@ -0,0 +1,17 @@
+const formFields = Array.from(document.querySelectorAll("#left input"));
+const rightContainer = document.getElementById("right");
+
+formFields.forEach((field) => {
+  field.addEventListener("input", () => {
+    const fieldName = field.parentElement.className;
+    jsonObject[fieldName] = field.value;
+    updateJsonDisplay();
+  });
+});
+
+function updateJsonDisplay() {
+  rightContainer.innerHTML = JSON.stringify({ ...jsonObject }, null, 2);
+}
+
+const jsonObject = {};
+updateJsonDisplay();
